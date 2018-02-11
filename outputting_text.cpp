5a04b128f49349a15b5e936fda168ae43b15278f
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-11 10:02:14 +0800

    Feat: New file outputting_text.cpp

---
+++ outputting_text.cpp
@@ -0,0 +1,23 @@
+/**
+ * outputting_text.cpp
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last update on 2018/02/10 PHT
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Starting program..." << flush;
+
+	cout << "This is a line of text." << endl;
+
+	cout << "Banana " << "Apple " << "Orange " << endl;
+
+	cout << "This is another line of text." << endl;
+
+	return 0;
+}
