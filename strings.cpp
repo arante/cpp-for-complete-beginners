43e5df3eeffb429c44b18b31f755962719fa91cd
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-11 10:33:49 +0800

    Feat: New file strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        string text1 = "Hello, how are you ";
+        string text2 = "Peter Parker";
+        string text3 = text1 + text2;
+
+        cout << text1 << text2 << endl;
+        //cout << text2 << endl;
+
+        cout << text3 << endl;
+
+        return 0;
+}
