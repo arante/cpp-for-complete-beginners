a2d377a90d658a39ff2fe47294dbbdfd52a11600
Billy Wilson Arante <arantebillywilson@gmail.com>
2018-02-25 13:59:02 +0800

    Feat: New file if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,43 @@
+/**
+ * if_else.cpp
+ *
+ * Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+ * Last update on 2018/02/25 PHT
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "1 - Add New User" << endl;
+        cout << "2 - Show All Users" << endl;
+        cout << "3 - Update User" << endl;
+        cout << "4 - Delete User" << endl;
+        cout << "5 - View User" << endl;
+        cout << "0 - Quit" << endl;
+
+        cout << "\nEnter your option: " << flush;
+
+        int option;
+        cin >> option; 
+
+        if (option == 1) {
+                cout << "\n** Add New User **" << endl;
+        } else if (option == 2) {
+                cout << "\n** Show All Users **" << endl;
+        } else if (option == 3) {
+                cout << "\n** Update User **" << endl;
+        } else if (option == 4) {
+                cout << "\n** Delete User **" << endl;
+        } else if (option == 5) {
+                cout << "\n** View User **" << endl;
+        } else if (option == 0) {
+                cout << "\nQuit" << endl;
+        } else {
+                cout << "\nInvalid option entered." << endl;
+        }
+
+        return 0;
+}
